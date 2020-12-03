def57d11d77297ba9038c6ffb3681d5e7b4aae3b
Tamara Rubilar <ta.rubilar@gmail.com>
2020-12-03 20:13:39 -0300

    Ejercicio 1, inicio constructor, desarrollo de herencia, polimorfismo y creación de métodos Student < Person

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -7,3 +7,25 @@ Ejercicio 1
 Utilizando los principios de Herencia y Polimorfismo, refactoriza el siguiente código para eliminar lainnecesaria cantidad de sentencias if en los métodos ask e introduce.(3 puntos )
     
 =end
+
+class Person
+    def initialize(first, last, age) #Inicio constructor 
+        @first_name = first
+        @last_name = last
+        @age = age
+       
+    end
+    def birthday
+        @age += 1
+    end
+end
+
+class Student < Person #Inicio de Herencia, Polimmorfismo y creación de métodos
+    def talk
+        puts "Aquí es la clase de programación con Ruby?"
+    end
+
+    def introduce
+        puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
