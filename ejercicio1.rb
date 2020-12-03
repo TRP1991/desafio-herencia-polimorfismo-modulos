71c20cc2d1fc523fbedf5016edcb0279e613572e
Tamara Rubilar <ta.rubilar@gmail.com>
2020-12-03 20:09:46 -0300

    Instrucciones ejercicio 1 y ejercicio 2

---
+++ ejercicio1.rb
@@ -0,0 +1,9 @@
+=begin
+ 
+Estudiante: Tamara Rubilar Pinilla 
+
+Ejercicio 1
+
+Utilizando los principios de Herencia y Polimorfismo, refactoriza el siguiente código para eliminar lainnecesaria cantidad de sentencias if en los métodos ask e introduce.(3 puntos )
+    
+=end
---
+++ ejercicio2.rb
@@ -0,0 +1,13 @@
+=begin
+ 
+Estudiante: Tamara Rubilar Pinilla 
+
+Ejercicio 2
+
+Crear la clase Animal con un atributo nombre. Agregar un método getter para el atributo nombre(1 punto)Crear las clases Ave, Mamifero e Insecto. Ambas heredan de Animal(1 punto)
+Crear las clases Pingüino, Paloma y Pato. Las tres heredan de Ave(1 punto)
+Crear las clases Perro, Gato y Vaca. Las tres heredan de Mamifero (1 punto)
+Crear las clases Mosca, Mariposa y Abeja. Las tres heredan de Insecto(1 punto)
+Incluye los módulos en cada subclase utilizando la instrucción include con el objetivo de definir las habilidades y tipo de alimentación de cada animal (2 puntos)
+    
+=end
