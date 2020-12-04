df54da5886813593d8a7e678d2efce1f2b7d1bce
TRP1991 <57776064+TRP1991@users.noreply.github.com>
2020-12-03 21:02:19 -0300

    Inicio módulos Ejercicio 2

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -11,3 +11,39 @@ Crear las clases Mosca, Mariposa y Abeja. Las tres heredan de Insecto(1 punto)
 Incluye los módulos en cada subclase utilizando la instrucción include con el objetivo de definir las habilidades y tipo de alimentación de cada animal (2 puntos)
     
 =end
+
+module Habilidades
+    module Volador
+        def volar
+            'Estoy volandooooo!'
+        end
+        def aterrizar
+            'Estoy cansado de volar, voy a aterrizar'
+        end
+    end
+    module Nadador
+        def nadar
+            'Estoy nadando!'
+        end
+        def sumergir
+            'glu glub glub glu'
+        end
+    end
+    module Caminante
+        def caminar
+            'Puedo caminar!'
+        end
+    end
+end
+module Alimentacion
+    module Herbivoro
+        def comer
+            'Puedo comer plantas!'
+        end
+    end
+    module Carnivoro
+        def comer
+            'Puedo comer carne!'
+        end
+    end
+end
