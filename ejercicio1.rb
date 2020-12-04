6b3aa737d231001655519f9899260cf642424845
TRP1991 <57776064+TRP1991@users.noreply.github.com>
2020-12-03 21:40:18 -0300

    Se incorpora método birthday
    Se corrigen puts.

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -9,40 +9,47 @@ Utilizando los principios de Herencia y Polimorfismo, refactoriza el siguiente c
 =end
 
 class Person
-    def initialize(first, last,) #Inicio constructor 
+    def initialize(first, last, age) #Inicio constructor 
         @first_name = first
         @last_name = last
+        @age = age
     end
+end
+
+def birthday
+ @age += 1
 end
 
 class Student < Person #Inicio de Herencia, Polimmorfismo y creación de métodos
     def talk
-        puts "Aquí es la clase de programación con Ruby?"
+        "Aquí es la clase de programación con Ruby?"
     end
 
     def introduce
-        puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+        "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
     end
 end
 
 class Teacher < Person #Inicio de Herencia, Polimmorfismo y creación de métodos
     def talk
-        puts "¡Bienvenido a la clase de programación con Ruby!"
+        "¡Bienvenido a la clase de programación con Ruby!"
     end
 
     def introduce
-        puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+        "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
     end
 end
 
 class Parent < Person #Inicio de Herencia, Polimmorfismo y creación de métodos
     def talk 
-        puts "Aquí es la reunión de apoderados?"
+        "Aquí es la reunión de apoderados?"
     end
 
     def introduce 
-        puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}."
+        "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}."
     end
 end
 
-#puts Student.new('Tamara','Rubilar').talk
+
+puts Student.new('Tamara','Rubilar', 29).talk
+puts Student.new('Tamara','Rubilar', 29).introduce
